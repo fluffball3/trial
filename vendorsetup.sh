038edfdf15ade2ce6b6bfa00dc398b03d9074656
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2024-01-25 23:04:31 +0530

    6th

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#COMMON_LUNCH_CHOICES
+#add_lunch_combo twrp_m33x-user
+#add_lunch_combo twrp_m33x-userdebug
+#add_lunch_combo twrp_m33x-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_m33x-user \
+    twrp_m33x-userdebug \
+    twrp_m33x-eng
